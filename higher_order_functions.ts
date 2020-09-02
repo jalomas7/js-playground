e3c35e99e17500405411324280582a30b180b1c9
Jacob Massengill <jacob.massengill@asurion.com>
2020-09-02 10:22:02 -0500

    adds higher_order_functions.ts

---
+++ higher_order_functions.ts
@@ -0,0 +1,14 @@
+const addN = (n: number) => (x: number) => {
+    return n + x;
+}
+
+const addTwoSimple = (x) => {
+    return 2 + x;
+}
+
+const addTwo = addN(2); // addTwo == addTwoSimple == addN(2)
+const addFive = addN(5);
+
+console.log(addTwoSimple(10)) // expect 12
+console.log(addTwo(10)) // expect 12
+console.log(addFive(10)) // expect 15
